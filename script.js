cd7e26da9e11a62ad9ecd8bc97798ec9c83a81c3
AccPurp <136247961+AccPurp@users.noreply.github.com>
2023-06-11 22:47:30 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+function changeOpacity0()
+{
+	var image = document.getElementById("image");
+	var opacity = document.getElementById("opacity");
+	image.style.opacity = 0;
+}
+function changeOpacity01()
+{
+	var image = document.getElementById("image");
+	var opacity = document.getElementById("opacity");
+	image.style.opacity = 0.1;
+}
+function changeOpacity02()
+{	
+	var image = document.getElementById("image");
+	var opacity = document.getElementById("opacity");
+	image.style.opacity = 0.2;
+	}
+function changeOpacity03()
+{
+	var image = document.getElementById("image");
+	var opacity = document.getElementById("opacity");
+	image.style.opacity = 0.3;
+}
+function changeOpacity04()
+{
+	var image = document.getElementById("image");
+	var opacity = document.getElementById("opacity");
+	image.style.opacity = 0.4;
+}
+function changeOpacity05()
+{
+	var image = document.getElementById("image");
+	var opacity = document.getElementById("opacity");
+	image.style.opacity = 0.5;
+}
+function changeOpacity06()
+{	
+	var image = document.getElementById("image");
+	var opacity = document.getElementById("opacity");
+	image.style.opacity = 0.6;
+}
+function changeOpacity07()
+{	
+	var image = document.getElementById("image");
+	var opacity = document.getElementById("opacity");
+	image.style.opacity = 0.7;
+}
+function changeOpacity08()
+{	
+	var image = document.getElementById("image");
+	var opacity = document.getElementById("opacity");
+	image.style.opacity = 0.8;
+}
+function changeOpacity09()
+{	
+	var image = document.getElementById("image");
+	var opacity = document.getElementById("opacity");
+	image.style.opacity = 0.9;
+}
+function changeOpacity1()
+{	
+	var image = document.getElementById("image");
+	var opacity = document.getElementById("opacity");
+	image.style.opacity = 1;
+}
